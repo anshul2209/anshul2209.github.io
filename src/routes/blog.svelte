63b1b15a187f238a6e823dc07fd33d678a835044
Anshul <anshul.bansal@olx.com>
2020-03-03 20:08:55 +0530

    added another blog post

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -7,6 +7,12 @@
 			subtitle: 'Have you ever wondered how the smartphones and fitness watches know when you are running, walking, cycling or just have been lethargic for a while . This post is aimed at demystify this and a walkthrough to build a system which can accurately detect the activity using some of the sensor data from the accelerometer and gyro meter in particular.', 
 			url: 'https://medium.com/analytics-vidhya/human-activity-tracker-system-2435b532b05e', 
 			cover: 'activity.jpg'
+		},
+		{
+			title: 'Deploying Sapper PWA using Github Pages: Step by Step Tutorial ( Part 1)', 
+			subtitle: 'Let\'s say you are done with the development of your application made using Svelte and Sapper and want to host it somewhere free of cost. Here is how to deploy your static Sapper PWA application using Github Pages.', 
+			url: 'https://medium.com/@hiiamanshul/deploying-sapper-pwa-using-github-pages-step-by-step-tutorial-part-1-1e9828dfe4de', 
+			cover: 'sveltesapper.jpg'
 		}
 	]
 </script>
