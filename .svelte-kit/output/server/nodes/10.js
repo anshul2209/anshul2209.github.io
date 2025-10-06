d4f3c6fcaa127c77d42045218c0c7e4111eeddad
Anshul Bansal <anshul.bansal@olx.in>
2025-10-06 10:36:38 +0530

    fixed vul

---
+++ .svelte-kit/output/server/nodes/10.js
@@ -0,0 +1,8 @@
+
+
+export const index = 10;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/videos/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/10.qxpGZI_z.js","_app/immutable/chunks/CV-qT96p.js","_app/immutable/chunks/D6YF6ztN.js","_app/immutable/chunks/IHki7fMi.js","_app/immutable/chunks/BCllW-BV.js","_app/immutable/chunks/C56cXcVU.js"];
+export const stylesheets = ["_app/immutable/assets/SectionHeader.BaaHnGmw.css","_app/immutable/assets/10.BvYrbh5v.css"];
+export const fonts = [];
---
+++ build/_app/immutable/chunks/C1MUUezh.js
@@ -0,0 +1,1 @@
+const g=(t,a,e,c)=>{gtag("event",t,{event_category:a,event_label:e,value:c})};export{g as t};
