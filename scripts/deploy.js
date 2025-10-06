eef5c13fa462699950729781aa54dc6b57071f3b
Anshul Bansal <anshul.bansal@olx.in>
2025-10-06 10:29:40 +0530

    added deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,16 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-    '__sapper__/export',
+    'build',
     {
-        branch: 'master',
-        repo: 'https://github.com/anshul2209/portfolio.git',
+        branch: 'gh-pages',
+        repo: 'https://github.com/anshul2209/anshul2209.github.io.git',
         user: {
             name: 'Anshul Bansal',
             email: 'anshulbansal2209@gmail.com'
         }
     },
     () => {
-        console.log('Deploy Complete!')
+        console.log('Manual Deploy Complete!')
     }
 )
